29f46b6fb3c641327023652cff36f327b6112896
tarscilla <almeidatarscilla@hotmail.com>
2025-09-19 22:42:49 -0300

    outro modelo de menu

--- tia-lu-food-apps.py
+++ tia-lu-food-apps.py
@@ -4,14 +4,12 @@ test3 = "isso é um teste3"
 
 print("Alooo")
 
-def menu():
-    menu = """
-    =================== MENU ===================
-    [1] \tCadastrar Item 
-    [2] \tAtualizar Item
-    [3] \tConsultar Itens
-    [4] \tDetalhes do Item
-    [0] \tSair
+menu = """
 
-    => """
-    return input(menu)
+[1] \tCadastrar Item 
+[2] \tAtualizar Item
+[3] \tConsultar Itens
+[4] \tDetalhes do Item
+[0] \tSair
+
+=> """
